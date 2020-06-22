c88c2045226494ccaa71b2af4871e3c7860ad148
YujiM-812 <chanmatsu812@gmail.com>
2020-06-22 15:51:51 +0900

    アップロード

---
+++ !test/GpioTest/GpioTest.ino
@@ -0,0 +1,84 @@
+/*******************************************************************
+    GpioTest.ino
+       
+    While you keep pushing button, LED is turned ON.
+
+*******************************************************************/
+
+
+#define	PIN_LED		(2)
+#define	PIN_BUTTON	(0)
+
+
+//=====================================
+// Initialize LED
+//=====================================
+void init_led()
+{
+    pinMode(PIN_LED, OUTPUT);
+    set_led_status(0);
+}
+
+//=====================================
+// Setup LED status(On or Off)
+//=====================================
+void set_led_status(int status) // status : 0->Off, not zero->On 
+{
+    if(status) { 
+        digitalWrite(PIN_LED, HIGH);    // turn on LED.
+    }
+    else {
+        digitalWrite(PIN_LED, LOW);     // turn off LED.
+    }
+}
+
+//=====================================
+// Initialize button
+//=====================================
+void init_button()
+{
+//    pinMode(PIN_BUTTON, OUTPUT); // 5v出力するって意味。
+    pinMode(PIN_BUTTON, INPUT); // 入力を受け取るという意味。BUG1
+}
+
+//=====================================
+// Get button status(On or Off)
+//=====================================
+int get_button_status()
+{
+	int button_status = digitalRead(PIN_BUTTON);
+    if(button_status == 0) { // If button is pushed
+        return  1;
+    }
+    else {
+        return  0;
+    }
+}
+
+//=====================================
+// Initialization
+//=====================================
+void setup()
+{
+    // Initialize LED
+    init_led();
+    // Initialize button
+    init_button();
+}
+
+//=====================================
+// Main loop
+//=====================================
+void loop() 
+{
+    int button_status;
+	button_status = get_button_status();
+	
+    if(button_status == 1) { // If button is pushed, turn on LED.
+        set_led_status(1);
+    }
+    else {
+        set_led_status(0);
+    }
+        
+}
